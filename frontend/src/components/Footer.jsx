8460f65e8382f6f1a8f6f469eaca6b36dc33e2c4
Hamza Rashid <163101534+SHRnumber@users.noreply.github.com>
2025-06-13 23:46:03 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,43 +1,34 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
+import { DoctorContext } from '../context/DoctorContext'
+import { AdminContext } from '../context/AdminContext'
+import { useNavigate } from 'react-router-dom'
 
-const Footer = () => {
+const Navbar = () => {
+
+  const { dToken, setDToken } = useContext(DoctorContext)
+  const { aToken, setAToken } = useContext(AdminContext)
+
+  const navigate = useNavigate()
+
+  const logout = () => {
+    navigate('/')
+    dToken && setDToken('')
+    dToken && localStorage.removeItem('dToken')
+    aToken && setAToken('')
+    aToken && localStorage.removeItem('aToken')
+  }
+
   return (
-    <div className='md:mx-10'>
-      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
-
-        <div>
-          <img className='mb-5 w-40' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-        </div>
-
-        <div>
-          <p className='text-xl font-medium mb-5'>COMPANY</p>
-          <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy policy</li>
-          </ul>
-        </div>
-
-        <div>
-          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-          <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>greatstackdev@gmail.com</li>
-          </ul>
-        </div>
-
+    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
+      <div className='flex items-center gap-2 text-xs'>
+        <img onClick={() => navigate('/')} className='w-36 sm:w-20 cursor-pointer' src={assets.admin_logo} alt="" />
+        <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
       </div>
-
-      <div>
-        <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto.com - All Right Reserved.</p>
-      </div>
-
+      <button onClick={() => logout()} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
     </div>
   )
 }
 
-export default Footer
+export default Navbar
